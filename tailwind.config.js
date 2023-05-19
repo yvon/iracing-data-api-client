28b56c29dabe109cf07abe0293d1036ac13dd550
Yvon Cognard <yvon@cognard.com>
2023-05-19 17:55:57 +0200

    Minor visual tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./templates/**/*.html.erb"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Whitaker', '-apple-system', 'BlinkMacSystemFont', 'HelveticaNeue-Light', 'Segoe UI', 'Helvetica Neue', 'Helvetica', 'Raleway', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', '-apple-system', 'BlinkMacSystemFont', 'HelveticaNeue-Light', 'Segoe UI', 'Helvetica Neue', 'Helvetica', 'Raleway', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol']
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
